f13843ef2fffb57912a2269280cbcab53e2767f0
Gabliz <lizzosgabriele@gmail.com>
2022-12-16 18:38:43 +0100

    added roboto type

--- kliz-compose/src/desktopMain/kotlin/it/gabliz/kliz/compose/theme/LizType.kt
+++ kliz-compose/src/desktopMain/kotlin/it/gabliz/kliz/compose/theme/LizType.kt
@@ -1,8 +1,11 @@
 package it.gabliz.kliz.compose.theme
 
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.platform.Font
+import androidx.compose.ui.unit.sp
 
 public val robotoFont : FontFamily = FontFamily(
     Font(resource = "font/roboto/Roboto-Thin.ttf", FontWeight.Thin),
@@ -18,4 +21,73 @@ public val gothamRoundedFont : FontFamily = FontFamily(
     Font(resource = "GothamRoundedBold_21016.ttf", FontWeight.Bold),
     Font(resource = "GothamRoundedBook_21018.ttf", FontWeight.Light),
     Font(resource = "GothamRoundedLight_21020.ttf", FontWeight.Thin),
+)
+
+public val robotoTypography : Typography = Typography(
+    defaultFontFamily = robotoFont,
+    h1 = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W300,
+        fontSize = 96.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W300,
+        fontSize = 60.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W400,
+        fontSize = 48.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W400,
+        fontSize = 34.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W400,
+        fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W500,
+        fontSize = 20.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp
+    ),
+    button = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp
+    ),
+    overline = TextStyle(
+        fontFamily = robotoFont,
+        fontWeight = FontWeight.W400,
+        fontSize = 10.sp
+    )
 )
