e69e8d9d0b2cfea549d816fb0c8e0334fbc7a917
Gabliz <lizzosgabriele@gmail.com>
2023-01-09 21:15:17 +0100

    changed some file and added debug module

---
+++ kliz-cmp/src/commonMain/kotlin/it/gabliz/kliz/common/LizColors.kt
@@ -0,0 +1,31 @@
+package it.gabliz.kliz.common
+
+import androidx.compose.ui.graphics.Color
+
+public val titanWhite : Color = Color(0xFFF9F9FF)
+public val doveGray : Color = Color(0xFF696969)
+public val silverChalice: Color = Color(0xFFA9A9A9)
+public val athensGray: Color = Color(0xFFF1F2F6)
+public val dodgerBlue: Color = Color(0xFF109CF1)
+public val indigo: Color = Color(0xFF4B70C9)
+public val dullLavender: Color = Color(0xFFA4A3E8)
+public val coral: Color = Color(0xFFFF7F50)
+public val vividTangerine: Color = Color(0xFFFFA07A)
+public val malibu: Color = Color(0xFF70A1FE)
+public val steelBlue: Color = Color(0xFF4682B4)
+public val silver: Color = Color(0xFFC0C0C0)
+public val geyser: Color = Color(0xFFCED7E0)
+public val cadetBlue: Color = Color(0xFFA4B0BE)
+public val breakerBay: Color = Color(0xFF5F9EA0)
+public val pastelGreen: Color = Color(0xFF7AED9F)
+public val jungleGreen: Color = Color(0xFF28B485)
+public val uniupo: Color = Color(0xFFC30C0F)
+public val persimmon: Color = Color(0xFFFF6448)
+public val darkBurgundy: Color = Color(0xFF82080A)
+public val radicalRed: Color = Color(0xFFFF4757)
+
+public val Purple200 :Color = Color(0xFFBB86FC)
+public val Purple500:Color = Color(0xFF6200EE)
+public val Purple700:Color = Color(0xFF3700B3)
+public val Teal200:Color = Color(0xFF03DAC5)
+
--- kliz-common/src/commonMain/kotlin/it/gabliz/kliz/common/util/Temp.kt
+++
@@ -1,9 +0,0 @@
-package it.gabliz.kliz.common.util
-
-import androidx.compose.ui.graphics.Color
-
-val TEMP1 = "TEMP"
-public val pastelGreen: Color = Color(0xFF7AED9F)
-public val jungleGreen: Color = Color(0xFF28B485)
-public val uniupo: Color = Color(0xFFC30C0F)
-public val persimmon: Color = Color(0xFFFF6448)
