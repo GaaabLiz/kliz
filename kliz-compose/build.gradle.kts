2bdf6bec45f2beea1b0087c2fb4401921044f039
gabliz <lizzosgabriele@gmail.com>
2026-02-23 18:02:40 +0100

    chore: bump version to 2.1.2

--- kliz-compose/build.gradle.kts
+++ kliz-compose/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "io.github.gaaabliz"
-version = "2.1.1"
+version = "2.1.2"
 
 kotlin {
     jvm()
--- kliz/build.gradle.kts
+++ kliz/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "io.github.gaaabliz"
-version = "2.1.1"
+version = "2.1.2"
 
 kotlin {
     jvm()
